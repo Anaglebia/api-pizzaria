c3a4e2d830743435412b365ab7ac1c166c2bec7e
Anaglebia <bia22.sp@icloud.com>
2023-11-10 15:33:20 -0300

    Adicionei banco de dados

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ const router = Router();
 
 router.get('/test', (req:Request, res:Response)=>{
     return res.json({Ok: true});
+    // throw new Error('Erro ao fazer a requisição!')
 })
 
 export {router};
